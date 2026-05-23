4b36846397a7d94ef048a4f2f69cf8b94b77e35b
Gabriel Gonçalves Maciel <gabrielggm60@gmail.com>
2026-05-23 16:21:21 -0300

    Commit inicial

---
+++ src/test/java/com/prototype/honda/api/PrototypeHondaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.honda.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeHondaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
